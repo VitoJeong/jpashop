3c4b8cbb25d76d18add43fd133b80dca4be1c4d9
vito <ckdghk987@gmail.com>
2021-05-28 20:24:17 +0900

    create : entities

---
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -0,0 +1,29 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Delivery {
+
+    @Id @GeneratedValue
+    @Column(name = "delivery_id")
+    private Long id;
+
+    @OneToOne(mappedBy = "delivery")
+    private Order order;
+
+    @Enumerated(EnumType.STRING)
+    private DeliveryStatus status; // [READY, COMP]
+
+    @Embedded
+    private Address address;
+
+}
