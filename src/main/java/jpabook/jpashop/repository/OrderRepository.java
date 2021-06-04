a2af85b25bb91e6368b82f0102ad69cf8e767c3c
vito <ckdghk987@gmail.com>
2021-06-04 21:33:37 +0900

    add : orderSearch

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -21,7 +21,13 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
-    //public List<Order> findAll(OrderSearch orderSearch) {
-    //
-    //}
+    public List<Order> findAll(OrderSearch orderSearch) {
+        return em.createQuery("SELECT o FROM Order o JOIN o.member m " +
+                "WHERE o.status = :status" +
+                "   AND m.name LIKE :name", Order.class)
+                .setParameter("status", orderSearch.getOrderStatus())
+                .setParameter("name", orderSearch.getMemberName())
+                .setMaxResults(1000) // 최대 1000건
+                .getResultList();
+    }
 }
