94659c450141ef7ae53c52ff0f71a85e35fc91eb
VitoJeong <ckdghk987@gmail.com>
2021-06-29 23:53:39 +0900

    add : orders api v1

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -21,12 +21,12 @@ public class JpashopApplication {
 		SpringApplication.run(JpashopApplication.class, args);
 	}
 
-	//	@Bean
-	//	Hibernate5Module hibernate5Module() {
-	//		Hibernate5Module hibernate5Module = new Hibernate5Module();
-	//		hibernate5Module.configure(Hibernate5Module.Feature.FORCE_LAZY_LOADING, true);
-	//		return hibernate5Module;
-	//	}
+	@Bean
+	Hibernate5Module hibernate5Module() {
+		Hibernate5Module hibernate5Module = new Hibernate5Module();
+//		hibernate5Module.configure(Hibernate5Module.Feature.FORCE_LAZY_LOADING, true);
+		return hibernate5Module;
+	}
 	// Hibernate5Module 기본설정 : 초기화 된 프록시 객체만 노출
 	// FORCE_LAZY_LOADING 옵션 : 양방향 연관관계를 계속 로딩하게 됨 한쪽에 @JsonIgnore 설정해줘야함
 	// -> Hibernate5Module 모듈을 사용하기 보다는 DTO로 변환해서 반환하는것이 좋은 방법!!
